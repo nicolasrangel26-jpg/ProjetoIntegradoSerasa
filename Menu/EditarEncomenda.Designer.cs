634be751ad354a565481f6e4ee6fc9553491c9d7
ALISON SERAFIM DE SOUZA <alison.ssouza1@senacsp.edu.br>
2026-05-08 16:04:25 -0300

    atts

--- Menu/EditarEncomenda.Designer.cs
+++ Menu/EditarEncomenda.Designer.cs
@@ -31,9 +31,7 @@
             btnEditarEnco = new Button();
             txtEdtObs = new TextBox();
             label1 = new Label();
-            label2 = new Label();
             label3 = new Label();
-            label4 = new Label();
             label5 = new Label();
             cbbSabooor = new ComboBox();
             label6 = new Label();
@@ -48,9 +46,9 @@
             // btnEditarEnco
             // 
             btnEditarEnco.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnEditarEnco.Location = new Point(102, 427);
+            btnEditarEnco.Location = new Point(93, 284);
             btnEditarEnco.Name = "btnEditarEnco";
-            btnEditarEnco.Size = new Size(132, 40);
+            btnEditarEnco.Size = new Size(113, 40);
             btnEditarEnco.TabIndex = 0;
             btnEditarEnco.Text = "Salvar";
             btnEditarEnco.UseVisualStyleBackColor = true;
@@ -58,58 +56,38 @@
             // 
             // txtEdtObs
             // 
-            txtEdtObs.Location = new Point(12, 366);
+            txtEdtObs.Location = new Point(30, 232);
             txtEdtObs.Name = "txtEdtObs";
-            txtEdtObs.Size = new Size(300, 23);
+            txtEdtObs.Size = new Size(237, 23);
             txtEdtObs.TabIndex = 5;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 20);
+            label1.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 74);
             label1.Name = "label1";
-            label1.Size = new Size(56, 18);
+            label1.Size = new Size(43, 16);
             label1.TabIndex = 6;
             label1.Text = "Sabor";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(12, 214);
-            label2.Name = "label2";
-            label2.Size = new Size(172, 18);
-            label2.TabIndex = 7;
-            label2.Text = "Quantidade (pizza)";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(12, 89);
+            label3.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 141);
             label3.Name = "label3";
-            label3.Size = new Size(65, 18);
+            label3.Size = new Size(49, 16);
             label3.TabIndex = 8;
             label3.Text = "Bebida";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(12, 280);
-            label4.Name = "label4";
-            label4.Size = new Size(183, 18);
-            label4.TabIndex = 9;
-            label4.Text = "Quantidade (bebida)";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(12, 348);
+            label5.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(30, 211);
             label5.Name = "label5";
-            label5.Size = new Size(104, 18);
+            label5.Size = new Size(80, 16);
             label5.TabIndex = 10;
             label5.Text = "Observação";
             // 
@@ -117,58 +95,58 @@
             // 
             cbbSabooor.DropDownStyle = ComboBoxStyle.DropDownList;
             cbbSabooor.FormattingEnabled = true;
-            cbbSabooor.Location = new Point(12, 41);
+            cbbSabooor.Location = new Point(12, 95);
             cbbSabooor.Name = "cbbSabooor";
-            cbbSabooor.Size = new Size(300, 23);
+            cbbSabooor.Size = new Size(121, 23);
             cbbSabooor.TabIndex = 11;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Georgia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(12, 154);
+            label6.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(12, 10);
             label6.Name = "label6";
-            label6.Size = new Size(67, 18);
+            label6.Size = new Size(50, 16);
             label6.TabIndex = 14;
             label6.Text = "Cliente";
             // 
             // nudEdtQuantSabor
             // 
-            nudEdtQuantSabor.Location = new Point(12, 235);
+            nudEdtQuantSabor.Location = new Point(157, 95);
             nudEdtQuantSabor.Name = "nudEdtQuantSabor";
-            nudEdtQuantSabor.Size = new Size(155, 23);
+            nudEdtQuantSabor.Size = new Size(120, 23);
             nudEdtQuantSabor.TabIndex = 15;
             // 
             // nudEdtQuantBebida
             // 
-            nudEdtQuantBebida.Location = new Point(12, 299);
+            nudEdtQuantBebida.Location = new Point(157, 163);
             nudEdtQuantBebida.Name = "nudEdtQuantBebida";
-            nudEdtQuantBebida.Size = new Size(155, 23);
+            nudEdtQuantBebida.Size = new Size(120, 23);
             nudEdtQuantBebida.TabIndex = 16;
             // 
             // cbbBebidas2
             // 
             cbbBebidas2.DropDownStyle = ComboBoxStyle.DropDownList;
             cbbBebidas2.FormattingEnabled = true;
-            cbbBebidas2.Location = new Point(12, 110);
+            cbbBebidas2.Location = new Point(12, 162);
             cbbBebidas2.Name = "cbbBebidas2";
-            cbbBebidas2.Size = new Size(300, 23);
+            cbbBebidas2.Size = new Size(121, 23);
             cbbBebidas2.TabIndex = 17;
             // 
             // cbbClientes
             // 
             cbbClientes.DropDownStyle = ComboBoxStyle.DropDownList;
             cbbClientes.FormattingEnabled = true;
-            cbbClientes.Location = new Point(12, 175);
+            cbbClientes.Location = new Point(12, 31);
             cbbClientes.Name = "cbbClientes";
-            cbbClientes.Size = new Size(300, 23);
+            cbbClientes.Size = new Size(273, 23);
             cbbClientes.TabIndex = 18;
             // 
             // EditarEncomenda
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(332, 489);
+            ClientSize = new Size(298, 336);
             Controls.Add(cbbClientes);
             Controls.Add(cbbBebidas2);
             Controls.Add(nudEdtQuantBebida);
@@ -176,9 +154,7 @@
             Controls.Add(label6);
             Controls.Add(cbbSabooor);
             Controls.Add(label5);
-            Controls.Add(label4);
             Controls.Add(label3);
-            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(txtEdtObs);
             Controls.Add(btnEditarEnco);
@@ -199,9 +175,7 @@
         private TextBox txtEdtQntBebida;
         private TextBox txtEdtObs;
         private Label label1;
-        private Label label2;
         private Label label3;
-        private Label label4;
         private Label label5;
         private ComboBox cbbSabooor;
         private ComboBox comboBox2;
